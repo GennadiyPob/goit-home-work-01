9de988f435686990b2d67988f30d98433a554651
GennadiyPob <137322270+GennadiyPob@users.noreply.github.com>
2023-07-09 13:56:01 +0300

    Update ex_8_extra

--- ex_8_extra.py
+++ ex_8_extra.py
@@ -5,6 +5,16 @@
 
 num = int(input('Enter 4-digit number :'))
 
-num_1 = num // 1000
-num_2 = num // 100
-num_3 = num // 10
+num_1 = num // 1000        #Spit first digit 
+num_2 = (num // 100) % 10  #Spit second digit  
+num_3 = (num // 10) % 10   #Spit third digit 
+num_4 = num % 10           #Spit fourth digit   
+
+print('num_1', num_1)
+print('num_2', num_2)
+print('num_3', num_3)
+print('num_4', num_4)
+
+average = (num_1 + num_2 + num_3 + num_4) / 4
+
+print('Average number of digits', average)
